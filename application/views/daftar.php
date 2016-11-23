823be2bd477fff5ead4404046b44baa212667e0d
Daneswara <daneswarajauhari@gmail.com>
2016-11-23 20:23:13 +0700

    add halaman daftar

---
+++ application/views/daftar.php
@@ -0,0 +1,297 @@
+<!doctype html>
+
+
+<html lang="en" class="no-js">
+    <head>
+        <title>Baby Land - Daftar</title>
+
+        <meta charset="utf-8">
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+        <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,300,100,700' rel='stylesheet' type='text/css'>
+
+
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/bootstrap.css" media="screen">	
+        <!-- REVOLUTION BANNER CSS SETTINGS -->
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/fullwidth.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/settings.css" media="screen" />
+
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/magnific-popup.css" media="screen">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/font-awesome.css" media="screen">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/animate.css" media="screen">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/style.css" media="screen">
+
+        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.migrate.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.magnific-popup.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.appear.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>/js/bootstrap.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.imagesloaded.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.isotope.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>/js/retina-1.1.0.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>/js/plugins-scroll.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>/js/waypoint.min.js"></script>
+
+        <!-- jQuery KenBurn Slider  -->
+        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.themepunch.revolution.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.countTo.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>/js/script.js"></script>
+
+    </head>
+    <body>
+
+        <!-- Container -->
+        <div id="container">
+            <!-- Header
+                ================================================== -->
+            <header class="clearfix">
+                <!-- Static navbar -->
+                <div class="navbar navbar-default navbar-static-top">
+                    <div class="top-line">
+                        <div class="container">
+                            <ul class="top-list">
+                                <li><a href="#"><i class="fa fa-shopping-cart"></i> Your Cart <span>(23)</span> Items</a></li>
+                                <li>
+                                    <form>
+                                        <input type="search" placeholder="Search..."/>
+                                        <button type="submit">
+                                            <i class="fa fa-search"></i>
+                                        </button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        <div class="navbar-collapse collapse">
+                            <ul class="nav navbar-nav">
+                                <li class="drop"><a class="active" href="index-2.html">Sewa</a>
+                                    <ul class="drop-down">
+                                        <li><a href="index-2.html">Home 1</a></li>
+                                        <li><a href="home2.html">Home 2</a></li>
+                                        <li><a href="home3.html">Home 3</a></li>
+                                        <li><a href="home4.html">Home 4</a></li>
+                                        <li><a href="home5.html">Home 5</a></li>
+                                        <li><a href="home6.html">Home 6</a></li>
+                                    </ul>
+                                </li>
+                                <li class="drop"><a href="#">Profil</a>
+                                    <ul class="drop-down">
+                                        <li class="drop"><a href="about.html">About</a>
+                                            <ul class="drop-down level3">
+                                                <li><a href="about.html">About 1</a></li>
+                                                <li><a href="about2.html">About 2</a></li>
+                                                <li><a href="about3.html">About 3</a></li>
+                                                <li><a href="about-me.html">About Me</a></li>
+                                                <li><a href="about-me2.html">About Me 2</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="drop"><a href="services.html">Services</a>
+                                            <ul class="drop-down level3">
+                                                <li><a href="services.html">Services 1</a></li>
+                                                <li><a href="services2.html">Services 2</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="pricing.html">Pricing Tables</a></li>
+                                        <li><a href="careers.html">Careers</a></li>
+                                        <li><a href="testimonials.html">Testimonials</a></li>
+                                        <li><a href="blank.html">Blank Page</a></li>
+                                        <li><a href="error.html">Error 404</a></li>
+                                        <li><a href="<?php echo base_url('login') ?>">Log in</a></li>
+                                    </ul>
+                                </li>
+                                <li class="logo"><a href="index-2.html"><img alt="" src="<?php echo base_url() ?>/images/logo.png"></a></li>
+                                <li class="drop"><a href="blog.html">Masuk</a>
+                                    <ul class="drop-down">
+                                        <li><a href="blog-fullwidth.html">Fullwidth</a></li>
+                                        <li><a href="blog-leftsidebar.html">Blog left sidebar</a></li>
+                                        <li><a href="blog-rightsidebar.html">Blog right sidebar</a></li>
+                                        <li class="drop"><a href="blog2-leftsidebar.html">Blog version2</a>
+                                            <ul class="drop-down level3">
+                                                <li><a href="blog2-leftsidebar.html">Left Sidebar</a></li>
+                                                <li><a href="blog2-rightsidebar.html">Right Sidebar</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="drop"><a href="blog-masonry.html">blog Masonry</a>
+                                            <ul class="drop-down level3">
+                                                <li><a href="blog-masonry.html">No Sidebar</a></li>
+                                                <li><a href="blog-masonry-lsidebar.html">Left Sidebar</a></li>
+                                                <li><a href="blog-masonry-rsidebar.html">Right Sidebar</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="single-post.html">Single post</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="portfolio-3col.html">Daftar</a>
+                                    <ul class="drop-down">
+                                        <li class="drop"><a href="portfolio-1col.html">Portfolio 1col</a>
+                                            <ul class="drop-down level3">
+                                                <li><a href="portfolio-1col.html">Version 1</a></li>
+                                                <li><a href="portfolio-1col-vers2.html">Version 2</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="drop"><a href="portfolio-2col.html">Portfolio 2col</a>
+                                            <ul class="drop-down level3">
+                                                <li><a href="portfolio-2col.html">Version 1</a></li>
+                                                <li><a href="portfolio-2col-vers2.html">Version 2</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="drop"><a href="portfolio-3col.html">Portfolio 3col</a>
+                                            <ul class="drop-down level3">
+                                                <li><a href="portfolio-3col.html">Version 1</a></li>
+                                                <li><a href="portfolio-3col-vers2.html">Version 2</a></li>
+                                                <li><a href="portfolio-3col-vers3.html">Version 3</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="drop"><a href="portfolio-4col.html">Portfolio 4col</a>
+                                            <ul class="drop-down level3">
+                                                <li><a href="portfolio-4col.html">Version 1</a></li>
+                                                <li><a href="portfolio-4col-vers2.html">Version 2</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="single-project.html">Single Project</a></li>
+                                        <li><a href="single-project2.html">Single Project 2</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <!-- End Header -->
+            <body>
+                <div id="container">
+                    <form class="log-in-form">
+                        <h1>Log in Details</h1>
+                        <label>E-mail*</label>
+                        <input type="text" name="mail" placeholder="email"/>
+                        <label>Password*</label>
+                        <input type="password" name="password" placeholder="password"/>
+                        <input type="checkbox"/> <span>Stay signed in</span>
+                        <input type="submit" value="Sign in"/>
+                        <p>You forgot your password</p>
+                        <a href="#">Recover Password</a>
+                    </form>
+                </div>
+            </body>
+
+
+            <!-- footer 
+                    ================================================== -->
+            <footer>
+
+                <div class="footer-line">
+                    <div class="container">
+                        <p><span class="span-one">BABYLAND 2016.</span> <span> All rights reserved.</span></p>
+
+                    </div>
+                </div>
+
+            </footer>
+            <!-- End footer -->
+        </div>
+        <!-- End Container -->
+
+        <!-- Contents of first window -->
+
+        <!--
+        ##############################
+         - ACTIVATE THE BANNER HERE -
+        ##############################
+        -->
+        <script type="text/javascript">
+
+            var tpj = jQuery;
+            tpj.noConflict();
+
+            tpj(document).ready(function() {
+
+                if (tpj.fn.cssOriginal != undefined)
+                    tpj.fn.css = tpj.fn.cssOriginal;
+
+                var api = tpj('.fullwidthbanner').revolution(
+                        {
+                            delay: 8000,
+                            startwidth: 1170,
+                            startheight: 864,
+                            onHoverStop: "off", // Stop Banner Timet at Hover on Slide on/off
+
+                            thumbWidth: 100, // Thumb With and Height and Amount (only if navigation Tyope set to thumb !)
+                            thumbHeight: 50,
+                            thumbAmount: 3,
+                            hideThumbs: 0,
+                            navigationType: "bullet", // bullet, thumb, none
+                            navigationArrows: "solo", // nexttobullets, solo (old name verticalcentered), none
+
+                            navigationStyle: "round", // round,square,navbar,round-old,square-old,navbar-old, or any from the list in the docu (choose between 50+ different item), custom
+
+
+                            navigationHAlign: "center", // Vertical Align top,center,bottom
+                            navigationVAlign: "bottom", // Horizontal Align left,center,right
+                            navigationHOffset: 30,
+                            navigationVOffset: 40,
+                            soloArrowLeftHalign: "left",
+                            soloArrowLeftValign: "center",
+                            soloArrowLeftHOffset: 40,
+                            soloArrowLeftVOffset: 0,
+                            soloArrowRightHalign: "right",
+                            soloArrowRightValign: "center",
+                            soloArrowRightHOffset: 40,
+                            soloArrowRightVOffset: 0,
+                            touchenabled: "on", // Enable Swipe Function : on/off
+
+
+                            stopAtSlide: -1, // Stop Timer if Slide "x" has been Reached. If stopAfterLoops set to 0, then it stops already in the first Loop at slide X which defined. -1 means do not stop at any slide. stopAfterLoops has no sinn in this case.
+                            stopAfterLoops: -1, // Stop Timer if All slides has been played "x" times. IT will stop at THe slide which is defined via stopAtSlide:x, if set to -1 slide never stop automatic
+
+                            hideCaptionAtLimit: 0, // It Defines if a caption should be shown under a Screen Resolution ( Basod on The Width of Browser)
+                            hideAllCaptionAtLilmit: 0, // Hide all The Captions if Width of Browser is less then this value
+                            hideSliderAtLimit: 0, // Hide the whole slider, and stop also functions if Width of Browser is less than this value
+
+
+                            fullWidth: "on",
+                            shadow: 1								//0 = no Shadow, 1,2,3 = 3 Different Art of Shadows -  (No Shadow in Fullwidth Version !)
+
+                        });
+
+
+                // TO HIDE THE ARROWS SEPERATLY FROM THE BULLETS, SOME TRICK HERE:
+                // YOU CAN REMOVE IT FROM HERE TILL THE END OF THIS SECTION IF YOU DONT NEED THIS !
+                api.bind("revolution.slide.onloaded", function(e) {
+
+
+                    jQuery('.tparrows').each(function() {
+                        var arrows = jQuery(this);
+
+                        var timer = setInterval(function() {
+
+                            if (arrows.css('opacity') == 1 && !jQuery('.tp-simpleresponsive').hasClass("mouseisover"))
+                                arrows.fadeOut(300);
+                        }, 3000);
+                    })
+
+                    jQuery('.tp-simpleresponsive, .tparrows').hover(function() {
+                        jQuery('.tp-simpleresponsive').addClass("mouseisover");
+                        jQuery('body').find('.tparrows').each(function() {
+                            jQuery(this).fadeIn(300);
+                        });
+                    }, function() {
+                        if (!jQuery(this).hasClass("tparrows"))
+                            jQuery('.tp-simpleresponsive').removeClass("mouseisover");
+                    })
+                });
+                // END OF THE SECTION, HIDE MY ARROWS SEPERATLY FROM THE BULLETS
+            });
+        </script>
+
+    </body>
+</html>
