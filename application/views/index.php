2437ba2740ffe111e51c6e666a6e49004b87d9bd
Daneswara <daneswarajauhari@gmail.com>
2016-11-28 20:36:41 +0700

    edit tampilan menu home

--- application/views/index.php
+++ application/views/index.php
@@ -10,34 +10,36 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-        <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,300,100,700' rel='stylesheet' type='text/css'>
+        
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>css/fonts/font-roboto.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>css/fonts/font-opensans.css">
 
 
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/bootstrap.css" media="screen">	
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>css/bootstrap.css" media="screen">	
         <!-- REVOLUTION BANNER CSS SETTINGS -->
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/fullwidth.css" media="screen" />
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/settings.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>css/fullwidth.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>css/settings.css" media="screen" />
 
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/magnific-popup.css" media="screen">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/font-awesome.css" media="screen">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/animate.css" media="screen">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/style.css" media="screen">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>css/magnific-popup.css" media="screen">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>css/font-awesome.css" media="screen">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>css/animate.css" media="screen">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>css/style.css" media="screen">
 
-        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.min.js"></script>
-        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.migrate.js"></script>
-        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.magnific-popup.min.js"></script>
-        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.appear.js"></script>
-        <script type="text/javascript" src="<?php echo base_url() ?>/js/bootstrap.js"></script>
-        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.imagesloaded.min.js"></script>
-        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.isotope.min.js"></script>
-        <script type="text/javascript" src="<?php echo base_url() ?>/js/retina-1.1.0.min.js"></script>
-        <script type="text/javascript" src="<?php echo base_url() ?>/js/plugins-scroll.js"></script>
-        <script type="text/javascript" src="<?php echo base_url() ?>/js/waypoint.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>js/jquery.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>js/jquery.migrate.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>js/jquery.magnific-popup.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>js/jquery.appear.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>js/bootstrap.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>js/jquery.imagesloaded.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>js/jquery.isotope.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>js/retina-1.1.0.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>js/plugins-scroll.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>js/waypoint.min.js"></script>
 
         <!-- jQuery KenBurn Slider  -->
-        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.themepunch.revolution.min.js"></script>
-        <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery.countTo.js"></script>
-        <script type="text/javascript" src="<?php echo base_url() ?>/js/script.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>js/jquery.themepunch.revolution.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>js/jquery.countTo.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() ?>js/script.js"></script>
 
     </head>
     <body>
@@ -51,22 +53,20 @@
                 <div class="navbar navbar-default navbar-static-top">
                     <div class="top-line">
                         <div class="container">
-                         <ul class="top-menu">
-                                <li><a href="#"><i class="fa fa-envelope"></i>yudistira96@gmail.com </a></li>
-                                <li><a href="#"><i class="fa fa-phone-square"></i>085746921137</a></li>
-                                <li><a href="#"><i class="fa fa-skype"></i>blablablabla</a></li>
+                            <ul class="top-menu">
+                                <li style="font-size: 25px; color: white">BabyLand</li>
                             </ul>
                             <ul class="top-list">
-                            	<li>
-                                    <form style="margin-right: 20px"> 
+                                <li>
+                                    <form style="margin-right: 20px">
                                         <input type="search" placeholder="Search..."/ style="width: 400px">
-                                        <span>
-                                              <a href="<?php echo base_url('index.php/Pencarian/index') ?>"><i class="fa fa-search" style="color:white;margin-left: 10px;font-size: 23px"></i></a>
+                                               <span>
+                                            <a href="<?php echo base_url('index.php/Pencarian/index') ?>"><i class="fa fa-search" style="color:white;margin-left: 10px;font-size: 23px"></i></a>
                                         </span>
-                                         
+
                                     </form>
                                 </li>
-                                <li><a href="<?php echo base_url('index.php/halamanKeranjang/index')?>"><i class="fa fa-shopping-cart"></i> Your Cart <span>(23)</span> Items</a></li>
+                                <li><a href="<?php echo base_url('index.php/halamanKeranjang/index') ?>"><i class="fa fa-shopping-cart"></i> Your Cart <span>(23)</span> Items</a></li>
                             </ul>
                         </div>
                     </div>
@@ -80,35 +80,17 @@
                         </div>
                         <div class="navbar-collapse collapse">
                             <ul class="nav navbar-nav">
-                                <li class="drop"><a href="<?php base_url('index.php') ?>">Home</a></li>
-                                <li class="drop"><a class="active" href="<?php echo base_url('index.php/Pengguna/index') ?>">Pemberitahuan</a>
-                                </li>
+                                <li class="drop"><a class="active" href="<?php base_url('index.php') ?>">Home</a></li>
+                                
                                 <li class="drop"><a href="<?php echo base_url('index.php/Profil/index'); ?>">Profil</a>
-                                    <!-- <ul class="drop-down">
-                                        <li class="drop"><a href="about.html">About</a>
-                                            <ul class="drop-down level3">
-                                                <li><a href="about.html">About 1</a></li>
-                                                <li><a href="about2.html">About 2</a></li>
-                                                <li><a href="about3.html">About 3</a></li>
-                                                <li><a href="about-me.html">About Me</a></li>
-                                                <li><a href="about-me2.html">About Me 2</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="drop"><a href="services.html">Services</a>
-                                            <ul class="drop-down level3">
-                                                <li><a href="services.html">Services 1</a></li>
-                                                <li><a href="services2.html">Services 2</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul> -->
                                 </li>
-                                <li class="logo"><a href="<?php base_url('index.php') ?>"><img alt="" src="<?php echo base_url() ?>/images/logo.png"></a></li>
+                                <li><a href="<?php echo base_url('index.php')?>">Transaksi</a></li>
+                                <li class="drop"><a href="<?php echo base_url('index.php/Pengguna/index') ?>">Pemberitahuan</a>
+                                </li>
                                 <li class="drop"><a href="<?php echo base_url('index.php/Login/index')?>">Masuk</a>
                                 </li>
                                 <li><a href="<?php echo base_url('index.php/Daftar/index')?>">Daftar</a>
-                                   
                                 </li>
-                                <li><a href="<?php echo base_url('index.php')?>">Transaksi</a></li>
                             </ul>
                         </div>
                     </div>
@@ -257,113 +239,6 @@
                                         </div>
                                     </div>
 
-                                    <div class="item">
-                                        <div class="row">
-
-                                            <div class="col-md-3 col-sm-6 col-xs-6">
-                                                <div class="product-post">
-                                                    <div class="product-gal">
-                                                        <img alt="" src="<?php echo base_url() ?>/upload/product2.jpg">
-                                                        <div class="hover-product">
-                                                            <a href="single-shop.html"><i class="fa fa-search"></i></a>
-                                                        </div>
-                                                    </div>
-                                                    <div class="product-content">
-                                                        <h3>Great Shoes</h3>
-                                                        <p class="price">Rp.  250.00</p>
-                                                    </div>
-                                                    <ul class="product-post-list">
-                                                        <li><a href="#"><i class=""></i></a></li>
-                                                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                                                        <li><a href="#"><i class=""></i></a></li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-
-                                            <div class="col-md-3 col-sm-6 col-xs-6">
-                                                <div class="product-post">
-                                                    <div class="product-gal">
-                                                        <img alt="" src="<?php echo base_url() ?>/upload/product4.jpg">
-                                                        <div class="hover-product">
-                                                            <a href="single-shop.html"><i class="fa fa-search"></i></a>
-                                                        </div>
-                                                    </div>
-                                                    <div class="product-content">
-                                                        <h3>Great Shoes</h3>
-                                                        <span class="rate">
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star-o"></i>
-                                                        </span>
-                                                        <p class="price">Rp.  250.00</p>
-                                                    </div>
-                                                    <ul class="product-post-list">
-                                                        <li><a href="#"><i class=""></i></a></li>
-                                                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                                                        <li><a href="#"><i class=""></i></a></li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-
-                                            <div class="col-md-3 col-sm-6 col-xs-6">
-                                                <div class="product-post">
-                                                    <div class="product-gal">
-                                                        <img alt="" src="<?php echo base_url() ?>/upload/product1.jpg">
-                                                        <div class="hover-product">
-                                                            <a href="single-shop.html"><i class="fa fa-search"></i></a>
-                                                        </div>
-                                                    </div>
-                                                    <div class="product-content">
-                                                        <h3>Great Shoes</h3>
-                                                        <span class="rate">
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star-o"></i>
-                                                        </span>
-                                                        <p class="price">Rp.  250.00</p>
-                                                    </div>
-                                                    <ul class="product-post-list">
-                                                        <li><a href="#"><i class=""></i></a></li>
-                                                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                                                        <li><a href="#"><i class=""></i></a></li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-
-                                            <div class="col-md-3 col-sm-6 col-xs-6">
-                                                <div class="product-post">
-                                                    <div class="product-gal">
-                                                        <img alt="" src="<?php echo base_url() ?>/upload/product3.jpg">
-                                                        <div class="hover-product">
-                                                            <a href="single-shop.html"><i class="fa fa-search"></i></a>
-                                                        </div>
-                                                    </div>
-                                                    <div class="product-content">
-                                                        <h3>Great Shoes</h3>
-                                                        <span class="rate">
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star"></i>
-                                                            <i class="fa fa-star-o"></i>
-                                                        </span>
-                                                        <p class="price">Rp.  250.00</p>
-                                                    </div>
-                                                    <ul class="product-post-list">
-                                                        <li><a href="#"><i class=""></i></a></li>
-                                                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                                                        <li><a href="#"><i class=""></i></a></li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-
-                                        </div>
-
-                                    </div>
 
                                 </div>
 
@@ -385,14 +260,6 @@
             <div class="footer-line">
                 <div class="container">
                     <p><span class="span-one">BABYLAND 2016.</span></p>
-                    <ul class="footer-social-icons">
-                        <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li><a class="rss" href="#"><i class="fa fa-rss"></i></a></li>
-                        <li><a class="google" href="#"><i class="fa fa-google-plus"></i></a></li>
-                        <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-                        <li><a class="pinterest" href="#"><i class="fa fa-pinterest"></i></a></li>                      
-                    </ul>
                 </div>
             </div>
 
