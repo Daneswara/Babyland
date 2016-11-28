38bbeeeedc577dbfd4eea8a023e58a069a96fdf9
Daneswara <daneswarajauhari@gmail.com>
2016-11-28 20:28:28 +0700

    edit tampilan menu daftar

--- application/views/HalamanDaftar.php
+++ application/views/HalamanDaftar.php
@@ -53,21 +53,19 @@
                     <div class="top-line">
                         <div class="container">
                             <ul class="top-menu">
-                                <li><a href="#"><i class="fa fa-envelope"></i>yudistira96@gmail.com </a></li>
-                                <li><a href="#"><i class="fa fa-phone-square"></i>085746921137</a></li>
-                                <li><a href="#"><i class="fa fa-skype"></i>blablablabla</a></li>
+                                <li style="font-size: 25px; color: white">BabyLand</li>
                             </ul>
                             <ul class="top-list">
                                 <li>
-                                    <form style="margin-right: 20px"> 
+                                    <form style="margin-right: 20px">
                                         <input type="search" placeholder="Search..."/ style="width: 400px">
-                                        <span>
-                                              <a href="<?php echo base_url('index.php/Pencarian/index') ?>"><i class="fa fa-search" style="color:white;margin-left: 10px;font-size: 23px"></i></a>
+                                               <span>
+                                            <a href="<?php echo base_url('index.php/Pencarian/index') ?>"><i class="fa fa-search" style="color:white;margin-left: 10px;font-size: 23px"></i></a>
                                         </span>
-                                         
+
                                     </form>
                                 </li>
-                                <li><a href="<?php echo base_url('index.php/halamanKeranjang/index')?>"><i class="fa fa-shopping-cart"></i> Your Cart <span>(23)</span> Items</a></li>
+                                <li><a href="<?php echo base_url('index.php/halamanKeranjang/index') ?>"><i class="fa fa-shopping-cart"></i> Your Cart <span>(23)</span> Items</a></li>
                             </ul>
                         </div>
                     </div>
@@ -82,40 +80,10 @@
                         <div class="navbar-collapse collapse">
                             <ul class="nav navbar-nav">
                                 <li class="drop"><a href="<?php echo base_url('index.php') ?>">Home</a></li>
-                                 <li class="drop"><a href="<?php echo base_url('index.php/Pengguna/index') ?>">Pemberitahuan</a></li>
+                                <li class="drop"><a href="<?php echo base_url('index.php/Login/index') ?>">Masuk</a>
                                 </li>
-                                <li class="drop"><a href="<?php echo base_url('index.php/Profil/index') ?>">Profil</a>
-                                    <ul class="drop-down">
-                                        <li class="drop"><a href="about.html">About</a>
-                                            <ul class="drop-down level3">
-                                                <li><a href="about.html">About 1</a></li>
-                                                <li><a href="about2.html">About 2</a></li>
-                                                <li><a href="about3.html">About 3</a></li>
-                                                <li><a href="about-me.html">About Me</a></li>
-                                                <li><a href="about-me2.html">About Me 2</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="drop"><a href="services.html">Services</a>
-                                            <ul class="drop-down level3">
-                                                <li><a href="services.html">Services 1</a></li>
-                                                <li><a href="services2.html">Services 2</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="pricing.html">Pricing Tables</a></li>
-                                        <li><a href="careers.html">Careers</a></li>
-                                        <li><a href="testimonials.html">Testimonials</a></li>
-                                        <li><a href="blank.html">Blank Page</a></li>
-                                        <li><a href="error.html">Error 404</a></li>
-                                        <li><a href="<?php echo base_url('index.php/Login/index')?>">Log in</a></li>
-                                    </ul>
+                                <li><a class="active" href="<?php echo base_url('index.php/Daftar/index') ?>">Daftar</a>
                                 </li>
-                                <li class="logo"><a href="<?php echo base_url('index.php')?>"><img alt="" src="<?php echo base_url() ?>/images/logo.png"></a></li>
-                                <li class="drop"><a href="<?php echo base_url('index.php/Login/index')?>">Masuk</a>
-                                </li>
-                                <li><a href="<?php echo base_url('index.php/Daftar/index')?>">Daftar</a>
-                                   
-                                </li>
-                                <li><a href="<?php echo base_url('index.php')?>">Transaksi</a></li>
                             </ul>
                         </div>
                     </div>
@@ -149,14 +117,6 @@
             <div class="footer-line">
                 <div class="container">
                     <p><span class="span-one">BABYLAND 2016.</span></p>
-                    <ul class="footer-social-icons">
-                        <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li><a class="rss" href="#"><i class="fa fa-rss"></i></a></li>
-                        <li><a class="google" href="#"><i class="fa fa-google-plus"></i></a></li>
-                        <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
-                        <li><a class="pinterest" href="#"><i class="fa fa-pinterest"></i></a></li>                      
-                    </ul>
                 </div>
             </div>
 
